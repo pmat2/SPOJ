62b914f15c95a30cf974437de96a46c1787a16e2
Pawel Matusiak <pawel.matusiak29@gmail.com>
2019-02-20 23:11:06 +0100

    readme init2

--- src/Korarek.java
+++ src/Korarek.java
@@ -7,28 +7,29 @@ import java.util.Scanner;
  */
 public class Korarek {
 
-    private static void coral(int n) {
-        if (n == 0) {
+    private static void coral(int n)
+    {
+        if(n == 0)
+        {
             return;
         }
-        if (n % 2 == 0) {
+        coral(n / 2);
+        if(n % 2 == 0)
+        {
             System.out.print("c");
         }
-        if (n % 2 != 0) {
+        else
+        {
             System.out.print("b");
         }
 
-        coral(n/2);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         coral(n);
-        System.out.println();
-
         sc.close();
     }
 }
